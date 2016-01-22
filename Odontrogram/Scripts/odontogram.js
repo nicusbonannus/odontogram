4a539e05f5796ff47d8d1faac7f573c3e49c7f7a
Nicolas <nicolas.bonanno@graion.com>
2016-01-22 15:24:00 -0300

    Agregue los dientes faltntes

--- Odontrogram/Scripts/odontogram.js
+++ Odontrogram/Scripts/odontogram.js
@@ -1,82 +1,89 @@
 ﻿var odontogramBox;
-function Odontogram(layerId) {
+function Odontogram(layerId, teeths) {
     this.LayerId = layerId,
+    this.TeethSection = { "U": 0, "R": 1, "B": 2, "L": 3, "C": 4 },
+    this.OdontrogramSections = [1,2,3,4],
     this.Layer = null,
+    this.Teeths = teeths,
     this.Initialize = function () {
         this.Layer = $("#" + layerId);
-        var divContainer = $("<div>", { id: "1", class: "section" });
-        for (i = 11; i <= 18; i++) {
-            var teethDiv = $("<div>", { id: i, class: "teeth" });
-            var teethPartLeft = $("<div>", {  class: "teethLeft" });
-            var teethPartRight = $("<div>", {class: "teethRight" });
-            var teethPartCenter = $("<div>", {class: "teethCenter" });
-            var teethPartBottom = $("<div>", {class: "teethBottom" });
-            var teethPartUpper = $("<div>", {class: "teethUpper" });
-
-            $(teethPartUpper).mousedown(function (e) {
-                if (e.button == 2) {
-
-                }
-            });
-            teethDiv.append(teethPartUpper);
-            teethDiv.append($("<div>", {class:"clear"}));
-            teethDiv.append(teethPartLeft);
-            teethDiv.append(teethPartCenter);
-            teethDiv.append(teethPartRight);
-            teethDiv.append($("<div>", { class: "clear" }));
-            teethDiv.append(teethPartBottom);
-
-
-            teethDiv.css('background-image', 'url(../Content/Images/AAAAA.png)');
-            divContainer.append(teethDiv);
+        var that = this;
+        $.each(this.OdontrogramSections, function (index, section) {
+            var divContainer = $("<div>", { id: "Section"+index, class: "section" });
             
+            $.each(that.Teeths, function (teethNumber, teethCode) {
+                //Teeth Number
+                var spanTeethNumber = $("<span>", {});
+                spanTeethNumber.html(teethNumber);
 
+                var teethContainer = $("<div>", { class: "teethContainer" });
+                var teethDiv = $("<div>", { id: teethNumber, class: "teeth" });
+                var teethPartLeft = $("<div>", { id: "L", class: "teethLeft" });
+                var teethPartRight = $("<div>", { id: "R", class: "teethRight" });
+                var teethPartCenter = $("<div>", { id: "C", class: "teethCenter" });
+                var teethPartBottom = $("<div>", { id: "B", class: "teethBottom" });
+                var teethPartUpper = $("<div>", { id: "U", class: "teethUpper" });
+
+                teethDiv.append(teethPartUpper);
+                teethDiv.append($("<div>", { class: "clear" }));
+                teethDiv.append(teethPartLeft);
+                teethDiv.append(teethPartCenter);
+                teethDiv.append(teethPartRight);
+                teethDiv.append($("<div>", { class: "clear" }));
+                teethDiv.append(teethPartBottom);
+
+
+                teethDiv.css('background-image', 'url(../Content/Images/' + teethCode + '.png)');
+                if (section == 1 || section == 2)
+                    teethContainer.append(spanTeethNumber);
+
+                teethContainer.append(teethDiv);
+
+                if (section == 3 || section == 4)
+                    teethContainer.append(spanTeethNumber);
+
+
+                divContainer.append(teethContainer);
+            });
+            that.Layer.append(divContainer);
+        });
+
+    }
+
+    this.SetOperation = function (action, section, teethNumber) {
+        this.Teeths[teethNumber] = this.GetCode(action,section,teethNumber);
+        $("#" + teethNumber).css('background-image', 'url(../Content/Images/' + this.Teeths[teethNumber] + '.png)');
+    },
+
+    this.GetCode = function (action, section, teethNumber) {
+        var teethCode = this.Teeths[teethNumber];
+        if (action == 'prequired') {
+            return this.ReplaceLetter(this.TeethSection[section], "C", teethCode);
+        } else if(action == 'pdone') {
+            return this.ReplaceLetter(this.TeethSection[section], "B", teethCode);
         }
 
-        this.Layer.append(divContainer);
-
+        return 'AAAAA';
+    },
+    this.ReplaceLetter = function (index, character,code) {
+        return code.substr(0, index) + character + code.substr(index + character.length);
     }
 }
 
 $(document).ready(function () {
-    odontogramBox = new Odontogram("odontogram");
+    var teeths = {
+        11: "AAAAA", 12: "ACCAA", 13: "ACCCA", 14: "AAAAA", 15: "AAAAA", 16: "CCCCC", 17: "ACACA", 18: "CCACC",
+        21: "AAAAA", 22: "ACCAA", 23: "ACCCA", 24: "AAAAA", 25: "AAAAA", 26: "CCCCC", 27: "ACACA", 28: "CCACC"
+    };
+
+    odontogramBox = new Odontogram("odontogram",teeths);
     odontogramBox.Initialize();
 
-    // Show menu when a list item is clicked
+    //Show menu when a list item is clicked
     $("#odontogram .teeth div").contextMenu({
         menu: 'myMenu'
     }, function (action, el, pos) {
-        alert(
-            'Action: ' + action + '\n\n' +
-            'Element ID: ' + el.parent().attr("id") + '\n\n'
-            );
+        odontogramBox.SetOperation(action, el.attr("id"), el.parent().attr("id"));
+
     });
-
-    // Disable menus
-    //$("#disableMenus").click(function () {
-    //    $('#myDiv, #myList UL LI').disableContextMenu();
-    //    $(this).attr('disabled', true);
-    //    $("#enableMenus").attr('disabled', false);
-    //});
-
-    //// Enable menus
-    //$("#enableMenus").click(function () {
-    //    $('#myDiv, #myList UL LI').enableContextMenu();
-    //    $(this).attr('disabled', true);
-    //    $("#disableMenus").attr('disabled', false);
-    //});
-
-    //// Disable cut/copy
-    //$("#disableItems").click(function () {
-    //    $('#myMenu').disableContextMenuItems('#cut,#copy');
-    //    $(this).attr('disabled', true);
-    //    $("#enableItems").attr('disabled', false);
-    //});
-
-    //// Enable cut/copy
-    //$("#enableItems").click(function () {
-    //    $('#myMenu').enableContextMenuItems('#cut,#copy');
-    //    $(this).attr('disabled', true);
-    //    $("#disableItems").attr('disabled', false);
-    //});
 });
